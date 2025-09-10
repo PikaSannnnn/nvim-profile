5c850f60845f583af35c1ddb311e92ec316eb9e4
Stephen Dong <dong.stephen11@gmail.com>
2025-09-10 15:52:04 -0700

    Updated coc and autopair settings

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,7 +1,10 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
+	config = function()
+		local npairs = require("nvim-autopairs")
+		npairs.setup({ check_ts = true, map_cr = true })
+	end,
 	-- use opts = {} for passing setup options
 	-- this is equivalent to setup({}) function
 }
--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,11 +1,29 @@
 return {
-	{
-		"neoclide/coc.nvim",
-		branch = "release",
-		lazy = false, -- load immediately
-		config = function()
-			-- optional: set some CoC options
-			vim.g.coc_global_extensions = { "coc-json", "coc-tsserver", "coc-pyright", "coc-clangd", "coc-html", "coc-css", "coc-markdownlint", "coc-go" }
-		end,
+	"neoclide/coc.nvim",
+	branch = "release",
+	lazy = false, -- load immediately
+	config = function()
+		-- optional: set some CoC options
+		vim.g.coc_global_extensions = {
+			"coc-json",
+			"coc-tsserver",
+			"coc-pyright",
+			"coc-clangd",
+			"coc-html",
+			"coc-css",
+			"coc-markdownlint",
+			"coc-go",
+		}
+	end,
+	keys = {
+		-- Navigate completion menu
+		{ "<TAB>", 'pumvisible() ? "\\<C-n>" : "\\<TAB>"', mode = "i", expr = true, noremap = true },
+		{ "<S-TAB>", 'pumvisible() ? "\\<C-p>" : "\\<S-TAB>"', mode = "i", expr = true, noremap = true },
+
+		-- Confirm selection
+		{ "<CR>", 'pumvisible() ? coc#_select_confirm() : "\\<CR>"', mode = "i", expr = true, noremap = true },
+
+		-- Trigger completion manually
+		{ "<C-Space>", "coc#refresh()", mode = "i", expr = true, noremap = true },
 	},
 }
